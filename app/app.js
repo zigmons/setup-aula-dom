9c37b40a6ee6bc01041caa2a4c542940d5ea5299
zigmons <rafael_sousa87@hotmail.com>
2022-11-01 21:52:09 -0300

    aula1

--- app/app.js
+++ app/app.js
@@ -38,10 +38,68 @@ const produtos = [
 const admin ={
     email: "admin@gmail.com",
     password: "admin",
+    nome:"Rafael",
+    sobrenome: "Sousa",
 }
 
 
 
 function showLogin(){
     document.getElementById("div-form-login").style.visibility="visible"
+}
+
+function login(){
+    let emaildigitado = document.getElementById("input-email").value
+    let senhadigitada = document.getElementById("input-password").value
+
+    if(emaildigitado === admin.email && senhadigitada === admin.password){
+        //ocultar formulario de login
+        document.getElementById("div-form-login").style.display = "none"
+        document.getElementById("btn-login").style.display = "none"
+        document.getElementById("img-profile").style.visibility="visible"
+
+        const welcome = document.createElement('h2');
+        welcome.innerText = `Bem Vindo, ${admin.nome}`;
+        document.getElementById("galeria-de-produtos").prepend(welcome)
+        
+        exibirProdutos()
+
+    }else{
+        alert("Email ou senha invalidos!")
+    }
+}
+
+const galeriaDeProdutos = document.getElementById("div-galeria")
+
+function exibirProdutos(){
+    for (i=0; i< produtos.length; i++){
+        // Criando um div para o produto
+        let produto = document.createElement("div");
+        produto.setAttribute("class", "produto")
+
+        // Criando , exibindo o nome do produto e adicionando o nome do produto ao div do produto
+        let nomeProduto = document.createElement("h3");
+        nomeProduto.innerText = produtos[i].nome;
+        produto.append(nomeProduto)
+        // Exibindo imagem
+        let imagemProduto = document.createElement("img");
+        imagemProduto.src = `assets/products/${produtos[i].imagem}`
+        produto.append(imagemProduto);
+        // // Preco
+        let precoProduto = document.createElement("p");
+        precoProduto.innerText =  `R$ ${produtos[i].preco}`;
+        produto.append(precoProduto);
+
+        let descricaoProduto = document.createElement("p");
+        descricaoProduto.innerText = produtos[i].descricao
+        produto.append(descricaoProduto)
+        
+
+
+
+        // Adicionando o produto a galeria
+        galeriaDeProdutos.append(produto);
+
+    }
+
 }
